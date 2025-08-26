e6dbb0bf6a049636aa28e59b817197ef82dd6f5e
Rahul <152004465+Gundarahull@users.noreply.github.com>
2025-08-26 12:48:46 +0530

    FIltering the Data_ USing where

---
+++ SQL/Basic/07_Filerting_data_WHERE.sql
@@ -0,0 +1,98 @@
+﻿-- WHERE
+-- 1 . Comparsion Operators
+-- compre two things
+-- Main SceNARIOS where we use the condition
+		-- 1.1 - Column1 = Column 2  >> users.id=customers.user_id
+		-- 1.2 : column = value      >> id= 1234
+		-- 1.3 : function = value    >> lower(name)='rahul' 
+		-- 1.4 : expression = value  >> mobileNumber%100 = 2
+		-- 1.5 : subQuery = value  
+
+-- First Opeartor --  " = " (Equals)
+select * from customers where country='Germany'
+
+-- Second Operator -- " != || <> " (Not Equals to)
+select * from customers where country !='Germany'
+
+-- Third Operator -- " > " (Greater than)
+select * from customers where score>500
+
+-- Fourth Operator -- " >= " (Greater than or Equal to)
+select * from customers where score>=500
+
+-- Five Operator -- " < " (Lesser than)
+select * from customers where score<500
+
+-- Sixth Operator -- " <= " (Lesser than or Equal to)
+select * from customers where score <=500
+
+-- ************************************************************************************************************
+
+-- 2 . Logical Operators
+-- AND operator fullfills the all conditions
+select * from customers where country='USA' and score >500 -- and id=7
+
+-- OR operator fullfills any one of the condition
+select * from customers where country='USA' OR score >500 or id=4
+
+-- NOT 
+
+	-- Flip each comparison to its opposite.
+	-- Flip AND ↔ OR.
+	-- Apply to the whole expression inside the NOT.
+
+select * from customers where NOT(score <500)
+
+-- same reverse
+select * from customers where score>=500
+
+
+-- ************************************************************************************************************
+
+-- 3 . Range Operators
+-- Betweeen (Lower Boundary -- Upper Boundary)(Inclusive) we need two things to do this
+select * from customers where score between 100 and 500
+
+-- we can write this also...
+select * from customers where score>=100 and score<=500
+
+-- ************************************************************************************************************
+
+-- 4 . MemberShip Operators
+-- IN (Which is in the LIST)
+select * from customers where country='Germany' or country='USA'
+
+-- In another Method
+select * from customers where country IN ('Germany','USA')
+
+-- ⚠️⚠️⚠️⚠️************************************************************************************************************
+
+-- 5. SEARCH Operator (IMPORTANT ONE)
+
+-- LIKE OPERATOR
+
+-- >> M%
+--		M should be at the last of the Word , then remaining anything it will be fine
+-- >> %in
+--		at the last should be 'in' before this anyhing will be fine
+-- >> %r%
+--		between the word , if 'r' presnts its satisfy
+-- >> __g%
+--		at the thitd position should be 'g' then anything is fine
+
+select * from customers where first_name LIKE 'M%'
+select * from customers where first_name LIKE '%n'
+
+select * from customers where first_name LIKE '%r%'
+
+select * from customers where first_name LIKE '__o%'
+
+
+SELECT '[' + first_name + ']' AS name, 
+       LEN(first_name) AS len
+FROM customers
+WHERE first_name LIKE '__o%';
+
+
+
+
---
+++ SQL/Basic/DML.sql
@@ -0,0 +1,74 @@
+﻿-- DML Lnaguague (DATA MAnipulation Language)
+
+INSERT INTO customers(
+	id,
+	first_name,
+	country,
+	score
+	)
+values(6,'Rahul','INDIA',900)
+
+
+INSERT INTO customers(id,first_name,country,score)
+values (7,'Sunny',null,897)
+
+-- without wriring the column name we can directly insert the values
+-- only iF we are matching with no of columns = no of values
+INSERT INTO customers 
+values (8,'Gunda','HYD',907)
+
+
+-- INSERT USING SELECT (Mind Bending...)
+
+
+CREATE TABLE persons (
+	id INT NOT NULL,
+	person_name VARCHAR(50) NOT NULL,
+	birth_date DATE ,
+	phone varchar(50) NOT NULL
+	CONSTRAINT pk_persons PRIMARY KEY (id)
+)
+
+-- **********************************************************
+
+-- (MAD) inserting the data into TBALE, using select
+-- we need to see and verify the column names need to set accordingly to the source TABLE to set in the table of TARGETED TABLE....
+INSERT INTO persons (id,person_name,birth_date,phone)
+select id,first_name,null,'phoneNumber' from customers
+
+-- **********************************************************
+
+-- ************* UPDATE************
+-- when u dont write the where condition it will update Whole TABLE
+
+-- *⚠️⚠️ Always use where to avoid UPDATING all rows unIntentionally
+
+UPDATE customers
+set score=NULL
+where id=6
+
+UPDATE customers 
+set score=0,
+	country='UK'
+where id=8
+
+update customers
+set score=0
+where score IS NULL
+
+-- ************ DELETE ***************
+
+DELETE FROM customers
+where id>5
+
+select * from customers
+
+delete from persons -- it removes all rows but still processes them individually.
+-- Does not reset auto-increment counters.
+
+truncate TABLE persons -- Removes all rows instantly.
+-- Resets identity/auto-increment to the seed value.
+
+-- truncate is faster than DELETE
+
+select * from persons 
