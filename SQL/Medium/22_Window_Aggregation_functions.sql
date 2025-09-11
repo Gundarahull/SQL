f36cda5cc765825546c48b0a35982fce135ac272
Rahul <152004465+Gundarahull@users.noreply.github.com>
2025-09-11 13:12:03 +0530

    WIndow_aggreagte_functions  || Sliding window || Running Total

---
+++ SQL/Medium/22_Window_Aggregation_functions.sql
@@ -0,0 +1,188 @@
+﻿-- Aggregate Window Functions
+
+-- 1. ✨ COUNT
+-- count(*) will ignore the nulls
+-- COUNT(1)==count(*)
+-- COUNT(COLUMN_NAME) WILL remove the nulls
+
+-- FIND the total number of orders
+
+select count(OrderID) as total_order from Sales.Orders
+
+-- additionally provide details such order if & orderdate
+
+select 
+	COUNT(OrderID) OVER() as total_orders,
+	OrderID,
+	OrderDate
+from Sales.Orders
+GROUP BY OrderID,OrderDate
+
+-- Find the total number of orders for each customer
+
+select 
+c.CustomerID,
+c.FirstName,
+count(o.sales) as orders_by_customer
+from Sales.Customers as c
+inner join Sales.Orders as o
+on c.CustomerID=o.CustomerID
+group by c.FirstName, c.CustomerID
+
+-- Find the total number of customers additionally provide all customers details
+
+select 
+	FirstName,
+	LastName,
+	Country,
+	Score,
+	COUNT(CustomerID) OVER() as total_cutomers
+from Sales.Customers
+
+
+-- Find the total number of scores for the customers
+
+select 
+--COUNT(COALESCE(Score,1)) as count_score
+COUNT(Score)
+from Sales.Customers
+
+-- Check whether the table 'Orders' contains any duplicate rows
+
+select 
+	CASE
+		WHEN count(DISTINCT(OrderID))=COUNT(OrderID) THEN 'No Duplicate'
+		ELSE 'Some duplicates'
+	END as check_duplicates,
+	count(DISTINCT(OrderID)) as totalCOut 
+from Sales.Orders
+
+-- PK have to refer while searching duplicate, if any specific column not mentioned
+
+select * from (
+	select 
+	OrderID,
+		count(*) OVER(PARTITION BY OrderID) as count_order
+	from Sales.OrdersArchive
+) as subi
+where count_order>1
+group by count_order,OrderID
+
+-- 2. ✨ SUM
+-- null will be ignored yaar...
+
+select * from Sales.Customers
+select SUM(Score) from Sales.Customers
+
+-- Find the total sales accross all orders
+-- and the total sales for each product
+-- additionally provide details such as order id, and order date
+
+select 
+	SUM(Sales) OVER() as total_sales ,
+	SUM(Sales) OVER(PARTITION BY ProductID) as product_Sales,
+	ProductID,
+	OrderID,
+	OrderDate
+from Sales.Orders
+
+-- Find the percantage contribution of each products sales to the total sales
+
+select 
+ROUND((product_Sales/CAST(total_sales as decimal(10,2)))*100,1) as percenti , total_sales,product_Sales
+	from
+		(
+			select 
+				SUM(Sales) OVER() as total_sales ,
+				SUM(Sales) OVER(PARTITION BY ProductID) as product_Sales,
+				ProductID,
+				OrderID,
+				OrderDate
+			from Sales.Orders
+		) as subi
+
+Select CAST(380 AS DECIMAL(10,1))
+
+
+-- 2. ✨ AVERAGE
+--Find the avergae sales across all orders
+-- and find the average sales for each product
+
+SELECT 
+	AVG(Sales) OVER() as avg_sales ,
+	Sales,
+	ProductID,
+	AVG(Sales) OVER(PARTITION BY ProductID) as product_avergae,
+	OrderID,
+	OrderDate
+from Sales.Orders
+
+-- Find the average scores of customers
+-- Additionally, provide details such as Customer ID and LastName
+
+SELECT 
+	AVG(COALESCE(Score,0)) Over() as avg_score,
+	Score,
+	CustomerID,
+	LastName
+from Sales.Customers
+
+--Find all orders where sales are higher than the average sales across all orders
+select * from (
+	select 
+		Sales, 
+		AVG(Sales) Over() as avg_sales
+	from Sales.Orders
+) as sumi
+where Sales>avg_sales
+
+-- 4. ✨ MIN || MAX
+
+-- Find the highest and lowest sales across all orders
+
+select
+	MAX(Sales) Over() as maxi,
+	MIN(Sales) Over() as mini,
+	MAX(Sales) Over(PARTITION BY ProductID) as pr_maxi,
+	MIN(Sales) Over(PARTITION BY ProductID) as pr_mini,
+	ProductID,
+	Sales,
+	OrderID,
+	OrderDate
+from Sales.Orders
+
+-- Show the employees with highest salaries
+
+Select FirstName,Salary  from Sales.Employees
+where Salary= (
+	SELECT MAX(Salary) from Sales.Employees
+)
+
+-- Running Total and Rolling Total
+-- RUN
+-- aggregate from begin to current point without dropping anything
+-- ROLL: (Shiting Window)
+-- aggregate within fixed range, when new thing comes ,it emit || throws the oldest one
+
+
+-- Calculate the Running_total || Moving avergaes of sales for each product over time
+
+select 
+	SUM(Sales) Over(PARTITION BY ProductID) as p_s,
+	SUM(Sales) Over(PARTITION BY ProductID Order by OrderDate) as running_out,
+
+	ProductID,
+	Sales,
+	OrderDate
+from Sales.Orders
+
+-- Calucalte the MA of sales for each product over time,inclusing only the next order
+
+-- Rolling TOTAL
+select 
+	AVG(Sales) OVER(PARTITION BY ProductID ORDER BY OrderDate  ROWS between current row and 1 following ) as ma_sales,
+	Sales,
+	ProductID
+from Sales.Orders
+
+
--- SQL/Medium/22_Window_Functions.sql
+++ SQL/Medium/22_Window_Functions.sql
@@ -1,4 +1,4 @@
--- Window or Analaytical Functions
+﻿-- Window or Analaytical Functions
 
 -- window is also grouping the things, but it will have the row level,
 -- wheras in the group by, it compress the row levels with unique ones
@@ -24,10 +24,158 @@ GROUP by ProductID
 
 -- find the total sales for each product, additionally provide details such order id and order date
 select 
-ProductID,
-SUM(Sales) as t_p,
-OrderID,
-OrderDate
+	ProductID,
+	SUM(Sales) as t_p,
+	OrderID,
+	OrderDate
 from 
 Sales.Orders
 group by ProductID,OrderID,OrderDate
+-- here while running this for productid, we are not getting sum of sales according to the product, its spreading like 
+-- 101 -10
+-- 102 -15
+-- 101 -20 (Again seeing 101), its not appropriate right, 
+/*
+		101	10	1	2025-01-01
+		102	15	2	2025-01-05
+		101	20	3	2025-01-10
+		105	60	4	2025-01-20
+		104	25	5	2025-02-01
+		104	50	6	2025-02-05
+		102	30	7	2025-02-15
+		101	90	8	2025-02-18
+		101	20	9	2025-03-10
+		102	60	10	2025-03-15
+*/
+-- here windows comes into the picture...
+
+-- 🎯♦️ Window functions returns a rsult for EACH ROW
+-- OVER() >> it give result to all rows
+-- it need to put beside the aggregate functions 
+-- PARTITION BY 'column_name' is like a group_by
+
+select 
+	OrderID,
+	OrderDate,
+	ProductId,
+	SUM(Sales) OVER(PARTITION BY ProductId)
+from Sales.Orders
+
+select 
+	department,
+	Gender,
+	FirstName,
+ SUM(Salary) OVER(PARTITION BY FirstName)
+from Sales.Employees
+
+
+-- 🏐🏐🏐 WINDOW Syntax
+
+
+-- WindowFunction(Aggr,Analytical and lead functions)- order caluse[OVER(PARTIATION BY "" ORDER BY "" )// insode over its an optional]
+
+-- ⚠️ OVER CLAUSE
+-- 1. PARTITION BY
+-- its like a group by (it will separetes by each unique one)
+-- we can use multiple columns for the partitions
+-- PARTITION BY (productID, productStatus)
+
+select 
+ProductID,
+OrderStatus,
+orderDate,
+sum(Sales) OVER(partition by productID,OrderStatus)
+from Sales.Orders
+
+-- Find the total Sales accross all orders, additionally provide deatsils such order id and order data
+
+select
+	SUM(sales) over() as acc_ord,
+	OrderID,
+	OrderDate
+from Sales.Orders
+
+
+-- find the total sales for each prodcut
+-- additionally provide details such as order id and order date
+
+select
+	SUM(Sales) OVER(PARTITION BY ProductID)as tp,
+	SUM(Sales) over() as acc_ts,
+	ProductID ,
+	OrderDate,
+	OrderID,
+	OrderStatus,
+	SUM(Sales) OVER(PARTITION BY ProductID,OrderStatus) as part_by_PID_OS
+from Sales.Orders
+
+-- within the one query we can use amny window to see the data separation according to the USE
+
+-- 2. ORDER BY (ordering within the window function)
+-- Rank each order based on their sales from highest to lowest,
+-- addiionally provide details such order if and order data
+
+select 
+	 RANK() OVER(ORDER by Sales desc) as rankk,
+	 Sales,
+	 OrderDate,
+	 OrderID
+from Sales.Orders
+
+-- 3. FRAME Clasue 
+
+-- subset of the window
+-- ⚠️⚠️ RULES
+-- FRAME Clause should be used in ORDER BY CLAUSE
+-- LOWER to HIGHER boundary value
+
+-- 2 FOLLOWING >> next 2 rows
+-- **--unBOUNDED Following is the LAST ROW--**
+-- **-- UNBOUNDED PRECEDING is the FIRST ROW --**
+-- Preceding : before the current row
+
+
+-- IF we use ORDER BY, default FRAME is
+-- ROWS BETWEEN UNBOUNDED PRECEDING  AND CURRENT ROW
+
+
+
+-- ⚠️⚠️⚠️ WINDOW FUNCTIONS RULES
+-- 1. ONly use in SELECT and ORDER BY
+-- 2. No Nested window functions
+-- 3. it will only execute after WHERE clause
+-- 4. we can use group by but same aggregation functions should be select and as well as in window function
+
+-- RANK CUSTOMERS BASED ON THEIR TOTAL SALES
+
+select * from Sales.Customers
+select * from Sales.Orders
+
+select 
+c.CustomerID,
+o.Sales,
+c.FirstName,
+SUM(sales) OVER(PARTITION BY c.CustomerID) as sales_per_person
+from Sales.Orders as o
+inner join Sales.Customers as c
+on o.CustomerID=c.CustomerID
+
+
+
+select
+*, RANK() OVER(ORDER BY per_person) 
+as RNKI from(
+	select 
+	c.CustomerID,
+	c.FirstName,
+	SUM(Sales) as per_person
+	from Sales.Orders as o
+	inner join Sales.Customers as c
+	on o.CustomerID=c.CustomerID
+	group by c.CustomerID, c.FirstName
+) as subi
+
+
+
+
+
