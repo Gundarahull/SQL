493c523853d78f18d0243a8784294019c707b6cf
Rahul <152004465+Gundarahull@users.noreply.github.com>
2025-09-21 18:55:31 +0530

    VIEWS Separating complex Queries

---
+++ SQL/Medium/29_VIEW.sql
@@ -0,0 +1,70 @@
+-- VIEWS
+-- DATA BASE Structure
+-- SQL SERVER -> DATABASE -> SCHEMAS -> TABLES + VIEWS 
+
+
+-- VIEWS also store the data, but we cant see them physically
+
+--  ABSTRACTION LEVEL 
+-- 3. VIEW LEVEL (to see the data)
+-- 2. LOGICAL LEVEL (to design the models, tables, procedurs, functions etc)
+-- 1. PHYSICAL LEVEL (Securing the Data, logs, caches etc)
+
+
+-- VIEWS -> Virtual table which deosnt store data, its a rsult of query
+-- its Abstrcation layer
+
+
+-- Central Logic
+
+-- We can use CTE as only one time in the main query , temporray 
+-- but VIEWs can use multiple_times, logic will store as virtual table
+
+---------------------------------------------------------------------------------------------------------
+
+
+-- CREATE || UPDATE || DROP
+
+-- Find the running total of sales per each Month
+
+-- ** Running Total is like CUMMALTIVE One**
+
+select 
+		MONTH(OrderDate) as monthi,
+		DATENAME(MONTH,OrderDate) as name,
+		SUM(Sales) OVER(PARTITION BY  MONTH(OrderDate) ORDER BY MONTH(OrderDate) asc) as running_total,
+		max(Sales) over()
+from Sales.Orders;
+
+-- Partition = “scope/group”
+-- Order = “sequence inside that group”
+
+-- here when we add, single aggregation function it has only value, so we cant display that right as single one, with all values
+-- so we need to use OVER()
+/*
+SELECT 
+    monthi,
+    name,
+    monthly_sales,
+    SUM(monthly_sales) OVER(ORDER BY monthi ASC) AS running_total
+FROM (
+    SELECT 
+        MONTH(OrderDate) AS monthi,
+        DATENAME(MONTH, OrderDate) AS name,
+        SUM(Sales) AS monthly_sales
+    FROM Sales.Orders
+    GROUP BY MONTH(OrderDate), DATENAME(MONTH, OrderDate)
+) AS subi;
+*/
+
+CREATE VIEW V_Monthly_Summary AS
+(
+	SELECT 
+	MONTH(OrderDate) AS monthi,
+	DATENAME(MONTH, OrderDate) AS name,
+	SUM(Sales) AS monthly_sales
+	FROM Sales.Orders
+	GROUP BY MONTH(OrderDate), DATENAME(MONTH, OrderDate)
+)
+
+select * from V_Monthly_Summary
---
+++ SQL/Medium/29_VIEWS_2.sql
@@ -0,0 +1,81 @@
+﻿-- DDL VIEWS
+
+-- ✨ CREATE
+/*
+
+*/
+
+CREATE VIEW Sales.V_Monthly_Summary AS
+(
+	SELECT 
+	MONTH(OrderDate) AS monthi,
+	DATENAME(MONTH, OrderDate) AS name,
+	SUM(Sales) AS monthly_sales
+	FROM Sales.Orders
+	GROUP BY MONTH(OrderDate), DATENAME(MONTH, OrderDate)
+)
+
+-- DROP
+
+DROP VIEW V_Monthly_Summary
+
+-- ALTER
+
+-- IN pGSQl we cn use Replace to alter the VIEWS
+-- but in the Microsoft SQL server we dont have that option
+-- 1. Drop
+-- 2. then Recreate a new one
+
+-- In the DISK storage, in the catalog, it will store META data and QUERY, it doesnt store any data 
+
+-- VIEW only stores the QUERY, not DATA
+
+-- 👍 USE CASE
+-- Hides the complexity like separating the Complex queries
+
+
+-- WHAT IS SCHEMA???????
+
+
+-- Provide a VIEW that combines details from Orders, products, customers and employess
+
+
+CREATE VIEW Sales.V_Combine AS (
+	select
+	o.CustomerID,
+	o.ProductID,
+	o.SalesPersonID
+	from Sales.Orders o
+	inner join Sales.Products p
+	on o.ProductID=p.ProductID
+	inner join Sales.Customers c
+	on o.CustomerID=c.CustomerID
+	inner join Sales.Employees e
+	on o.SalesPersonID=e.EmployeeID
+)
+
+
+
+-- 👍USE CASE 
+
+-- DATE Security, not tto show all columns to ALl persons
+-- ony particluar columsn, by creating VIEW
+
+-- Provide a View for the EU sales Team combines detaild from all tables
+-- exeildes data releated to the USA
+
+CREATE VIEW Sales.V_Combine_ExclUSA AS (
+	select
+	o.CustomerID,
+	o.ProductID,
+	o.SalesPersonID,
+	c.Country
+	from Sales.Orders o
+	inner join Sales.Products p
+	on o.ProductID=p.ProductID
+	inner join Sales.Customers c
+	on o.CustomerID=c.CustomerID
+	inner join Sales.Employees e
+	on o.SalesPersonID=e.EmployeeID
+	where c.Country NOT IN  ('USA')
+)
