a61632e119d3ee1aa0932764f35fd349b962e39b
Rahul <152004465+Gundarahull@users.noreply.github.com>
2025-09-23 10:11:35 +0530

    SubQuery Vs CTEs Vs CTAS Vs VIEWs Vs TEMP -- Apart from the Performace VIEW is Winner

---
+++ SQL/Medium/32_SubQuery_Vs CTEs Vs CTAS Vs VIEWs Vs TEMP.sql
@@ -0,0 +1,16 @@
+﻿-- Comparsions
+
+-- SubQuery Vs CTEs Vs CTAS Vs VIEWs Vs TEMP
+
+
+-- VIEW won the Match With 1st RAnk 🚩
+-- BUt it takes TIME, 
+-- while fetching first it will go to VIEW, VIEW will fetch the details from the Original Table
+-- this is two times doing the things,
+
+-- RAther than this fetching the result directly from the Tables will be the good IDEA...
+-- Refer Image for Why???
+
+-- Then CTES , Production grade dont USE More than 5 CTES
+
+-- SubQuery
